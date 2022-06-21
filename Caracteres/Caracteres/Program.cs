90075f054a323fc86a0d7d3b7e3071d504c42484
Lucas Comora <101115584+LComora@users.noreply.github.com>
2022-06-21 18:27:09 -0300

    Add files via upload

---
+++ Caracteres/Caracteres/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using static System.Console;
+
+namespace Caracteres
+{
+    internal class Program
+    {
+        public static string TrocaPalavra(string x)
+        {
+            char[] charArray = x.ToCharArray();
+            string troca = String.Empty;
+            for(int i = charArray.Length - 1; i >= 0; i--)
+            {
+                troca += charArray[i];
+            }
+            return troca;
+        }
+        static void Main(string[] args)
+        {
+            WriteLine("Digite a palavra a ser invertida: ");
+            string original = ReadLine();
+            string trocada = TrocaPalavra(original);
+            WriteLine($"Palavra digitada: {original}");
+            WriteLine($"Palavra invertida: {trocada}");
+        }
+    }
+}
