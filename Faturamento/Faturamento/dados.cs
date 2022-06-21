9600c1f8f0de18ee7235757a9b53569ecadaf3bc
Lucas Comora <101115584+LComora@users.noreply.github.com>
2022-06-21 18:28:56 -0300

    Add files via upload

---
+++ Faturamento/Faturamento/dados.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Faturamento
+{
+    public class dados
+    {
+        public int dia { get; set; }
+        public double valor { get; set; }
+    }
+}
